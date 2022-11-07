dd61a1b5802f3f64998f808fa9a08f25c9eed992
DESKTOP-J4GBE9I\Lavakumar <lavakumar4422@gmail.com>
2022-11-07 23:26:21 +0100

    intial commit

--- MoviesConsole/Program.cs
+++ MoviesConsole/Program.cs
@@ -6,11 +6,32 @@ namespace MoviesConsole
     {
         static void Main(string[] args)
         {
-            var context = new Movies_W3Context();
-            var moviesList = context.Movies;
-            foreach (var movie in moviesList)
+            while(true)
             {
-                Console.WriteLine(movie.MovTitle +" "+ movie.MovYear);
+                Console.WriteLine("Choose an option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 0:
+                        Console.WriteLine("Entered case 0");
+                        break;
+                    default:
+                        Console.WriteLine("Exiting...");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+            var context = new Movies_W3Context();
+            var moviesList = context.Movies.
+                                     Where(m => m.MovYear > 1995);
+            
+            if(!moviesList.Equals(null))
+            {
+                foreach (var movie in moviesList)
+                {
+                    Console.WriteLine(movie.MovTitle + " " + movie.MovYear);
+                }
             }
         }
     }
